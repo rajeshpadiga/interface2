8860aed709124a68a6ed7c48f426438fed6cf3ed
Rajesh <padigarajesh219@gmail.com>
2021-11-12 12:45:55 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Demo
+{
+    interface IMath
+    {
+        int add(int x, int y);
+        int sub(int x, int y);
+    }
+
+    class Math:IMath
+    {
+        public int add(int x, int y)
+        { return x + y; }
+
+        public int sub(int x, int y)
+        { return x - y; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            IMath math = new Math();
+            Console.WriteLine(math.add(10, 20));
+        }
+    }
+}
